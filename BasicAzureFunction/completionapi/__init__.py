0cf00346f54868d8eb594954beea7dbf9305d9b7
dipu157 <dipu157@gmail.com>
2023-02-16 10:58:09 +0600

    section 4 complete

--- BasicAzureFunction/completionapi/__init__.py
+++ BasicAzureFunction/completionapi/__init__.py
@@ -2,7 +2,10 @@ import logging
 import openai
 import azure.functions as func
 
-secret_key = "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"
+# sample request
+# {"model":"text-davinci-003","prompt":"give me a slogan for a cookie company", "max_tokens":200,"temperature":0} 
+
+secret_key = "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"
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -11,12 +14,13 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     openai.api_key = secret_key
     # get variable from the http body
     req_body = req.get_json()
+    logging.info(type(req_body))
     # call the openAI API
     output = openai.Completion.create(
         model = req_body['model'],
         prompt = req_body['prompt'],
-        max_token = req_body['max_tokens'],
-        temperature = ["temperature"]
+        max_tokens = req_body['max_tokens'],
+        temperature = req_body["temperature"]
 
     )
     # format the response
